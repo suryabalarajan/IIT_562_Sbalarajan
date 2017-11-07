adbef3b70f0e3046ed095903c9d6e7dd2677251f
suryabalarajan <31295693+suryabalarajan@users.noreply.github.com>
2017-11-07 16:52:50 -0600

    Create server.js

---
+++ Assignments/assignment7/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser());
+app.use(methodOverride('_method'));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get('/users', users.findAll);
+app.get('/users/:id', users.findById);
+app.get('/users/:id/reminders', users.findReminders);
+app.get('/users/:id/reminders/:rid', users.findremindersbyID);
+app.get('/adduser', users.getnewuser);
+app.post('/adduser', users.adduser)
+app.get('/users/:id/deleteuser', users.deleteuser);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
+
